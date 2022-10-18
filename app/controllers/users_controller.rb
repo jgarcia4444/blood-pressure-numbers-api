62adeeb682b5a0a74edcd822b7b34a26212b5ae1
Jake Garcia <bballwiz4@gmail.com>
2022-10-17 18:45:45 -0700

    Added in the logic to the create actio inside the users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,61 @@
 class UsersController < ApplicationController
     def create
+        if params[:new_user_info]
+            new_user_info = params[:new_user_info]
+            if new_user_info[:email]
+                email = new_user_info[:email]
+                if new_user_info[:password]
+                    password = new_user_info[:password]
+                    created_user = User.create(email: email.downcase, password: password)
+                    if created_user.valid?
+                        render :json => {
+                            success: true,
+                            userInfo: {
+                                userId: created_user.id,
+                                email: created_user.email
+                            }
+                        }
+                    else
+                        general_error = {
+                            errorType: "GENERAL",
+                            message: "There was an error saving your user information."
+                        }
+                        render :json => {
+                            success: false,
+                            errors: [general_error],
+                            backendErrorData: created_user.errors.full_messages
+                        }
+                    end
+                else
+                    no_pasword = {
+                        errorType: "PASSWORD",
+                        message: "Password cannot be left blank"
+                    }
+                    render :json => {
+                        success: false,
+                        errors: [no_pasword]
+                    }
+                end
+            else
+                no_email = {
+                    errorType: "EMAIL",
+                    message: "Email cannot be left blank"
+                }
+                render :json => {
+                    success: false,
+                    errors: [no_email]
+                }
+            end
+        else
+            general_error = {
+                errorType: "GENERAL", 
+                message: "Users information was not sent properly."
+            }
+            render :json => {
+                success: false,
+                errors: [general_error]
+            }
+        end
     end
 
     private
