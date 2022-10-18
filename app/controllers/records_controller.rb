cf9ac309458306ca6b9e834077a1900316077c43
Jake Garcia <bballwiz4@gmail.com>
2022-10-18 10:23:06 -0700

    Added logic to the records controller index action. This controller sends back errors based off proper identification being sent and a user being found. This controller also uses a newly created instance method of the user model that formats the record information to fit the frontend naming conventions.

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,4 +1,34 @@
 class RecordsController < ApplicationController
     def index
+        if params[:user_id]
+            user_id = params[:user_id]
+            found_user = User.find(id: user_id)
+            if found_user
+                formatted_records = found_user.format_user_records
+                render :json => {
+                    success: true,
+                    records: formatted_records
+                }
+            else 
+                find_user_error = {
+                    errorType: "USER_INFO",
+                    message: "A user was not found with the given identifier."
+                }
+                render :json => {
+                    success: false,
+                    errors: [find_user_error]
+                }
+            end
+        else
+            info_error = {
+                errorType: "PARAMS",
+                message: "Proper identification was not sent with the request. Records are unable to be retrieved."
+            }
+            render :json => {
+                success: false,
+                errors: [info_error]
+            }
+        end
     end
+    def create
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,21 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :records
+
+
+    def format_user_records
+        if self.records.count == 0
+            return []
+        else
+            self.records.map do |record|
+                {
+                    systolic: record.systolic,
+                    diastolic: record.diastolic,
+                    notes: record.notes,
+                    rightArmRecorded: record.right_arm_recorded
+                }
+            end
+        end
+    end
+
 end
