0fdc09167ea42b072319c0532c7b94c8296ac3fb
Jake Garcia <bballwiz4@gmail.com>
2023-02-25 20:21:35 -0800

    Added in the logic to update an existing day_streak record.

--- app/controllers/day_streaks_controller.rb
+++ app/controllers/day_streaks_controller.rb
@@ -45,6 +45,59 @@ class DayStreaksController < ApplicationController
     end
 
     def update
+        if params[:update_info]
+            update_info = params[:update_info]
+            if update_info[:user_id]
+                user_id = update_info[:user_id].to_i
+                user = User.find_by(id: user_id)
+                if user
+                    day_streak = DayStreak.find_by(user_id: user.id)
+                    if day_streak
+                        days_plus_one = day_streak + 1
+                        updated_expiration = DayStreak.configure_expiration
+                        day_streak.update(days: days_plus_one, expires_at: updated_expiration)
+                        if day_streak.valid?
+                        else
+                            render :json => {
+                                success: false,
+                                error: {
+                                    message: "There was an error updating the day streak.",
+                                    errors: day_streak.errors.full_messages,
+                                }
+                            }
+                        end
+                    else
+                        render :json => {
+                            success: false,
+                            error: {
+                                message: "Could not find the day streak associated with the user."
+                            }
+                        }
+                    end
+                else
+                    render :json => {
+                        success: false,
+                        error: {
+                            message: "A user was not found with the given user id."
+                        }
+                    }
+                end
+            else
+                render :json => {
+                    success: false,
+                    error: {
+                        message: "A user id must be present to complete this request."
+                    }
+                }
+            end
+        else
+            render :json => {
+                success: false,
+                error: {
+                    message: "Information was not formatted properly when sent to this route."
+                }
+            }
+        end
     end
 
     def show
