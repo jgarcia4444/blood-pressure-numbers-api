a8e48a8b090733833f53085379b1337783f9acf9
Jake Garcia <bballwiz4@gmail.com>
2023-03-01 14:27:12 -0800

    Fixed the add_column call.

--- db/migrate/20230301213849_add_default_to_users.rb
+++ db/migrate/20230301213849_add_default_to_users.rb
@@ -1,6 +1,6 @@
 class AddDefaultToUsers < ActiveRecord::Migration[7.0]
   def change
     remove_column :users, :code_verified
-    add_column :users, :code_verified, default: false
+    add_column :users, :code_verified, :string, default: false
   end
 end
