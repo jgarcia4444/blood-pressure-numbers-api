8695ff18120e8f04716cc626fff8548a679be693
Jake Garcia <bballwiz4@gmail.com>
2023-03-01 12:21:44 -0800

    Obligatory commit

--- config/routes.rb
+++ config/routes.rb
@@ -17,9 +17,9 @@ Rails.application.routes.draw do
   delete 'records/delete/:user_id/:record_id', to: 'records#destroy'
   put 'records/update', to: 'records#update'
 
-  get '/day-streak/:user_id', to: 'day_streaks#show'
-  post '/day-streaks', to: "day_streaks#create"
-  put '/day-streak', to: "day_streaks#update"
+  get 'day-streak/:user_id', to: 'day_streaks#show'
+  post 'day-streaks', to: "day_streaks#create"
+  put 'day-streak', to: "day_streaks#update"
 
-  get '/records/filter/date-range/:from_date/:to_date/:user_id', to: "records#filtered_records"
+  get 'records/filter/date-range/:from_date/:to_date/:user_id', to: "records#filtered_records"
 end
