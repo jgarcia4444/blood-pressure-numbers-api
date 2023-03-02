dcce5286db79876606975abd4912198d3cef2829
Jake Garcia <bballwiz4@gmail.com>
2023-03-01 16:29:19 -0800

    dropped and readded table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_26_021310) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_01_213849) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "day_streaks", force: :cascade do |t|
     t.integer "days"
     t.datetime "expires_at"
@@ -48,7 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_26_021310) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "code_verified", default: false
+    t.string "code_verified", default: "f"
   end
 
 end
