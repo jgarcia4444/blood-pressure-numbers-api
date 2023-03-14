2f3df02d543406a38233c95308fbd79f6a3f6bf1
Jake Garcia <bballwiz4@gmail.com>
2023-03-13 18:17:25 -0700

    Added in the contorller logic to create a forum message.

--- app/controllers/forum_messages_controller.rb
+++ app/controllers/forum_messages_controller.rb
@@ -1,5 +1,69 @@
 
 class ForumMessagesController < ApplicationController
+
+    def create
+        if params[:message_info]
+            message_info = params[:message_info]
+            if message_info[:username]
+               username = message_info[:username]
+               user = User.find_by(username: username)
+               if user
+                    if message_info[:message]
+                        message = message_info[:message]
+                        created_message = ForumMessage.create(user_id: user.id, message: message)
+                        if created_message.valid?
+                            render :json => {
+                                success: true,
+                                message: {
+                                    message: message,
+                                    username: username,
+                                    createdAt: created_message.created_at
+                                }
+                            }
+                        else
+                            render :json => {
+                                success: false,
+                                error: {
+                                    message: "There was an error creating the forum message.",
+                                    errors: created_message.errors.full_messages
+                                }
+                            }
+                        end
+                    else
+                        render :json => {
+                            success: false,
+                            error: {
+                                message: "Message info was not sent with the request."
+                            }
+                        }
+                    end
+
+               else
+                render :json => {
+                    success: false,
+                    error: {
+                        message: "No user was found with the given username."
+                    }
+                }
+               end
+            else
+                render :json => {
+                    success: false,
+                    error: {
+                        message: "A username was not found associated with this message."
+                    }
+                }
+            end
+        else
+            render :json => {
+                succes: false,
+                error: {
+                    message: "Message info was not sent properly."
+                }
+            }
+        end
+    end
+
     def index
         messages = ForumMessage.all 
         if messages.count == 0 
@@ -15,4 +79,5 @@ class ForumMessagesController < ApplicationController
             }
         end
     end
+
 end
